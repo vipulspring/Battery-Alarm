8a439196ac54c25409f9060a11a53281f3fbf66d
vipulsingh <vipulspring@gmail.com>
2023-05-14 20:22:58 +0530

    14th May

--- app/src/main/java/com/example/batteryalarm/MainActivity.java
+++ app/src/main/java/com/example/batteryalarm/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.batteryalarm.arcprogressbar.ColorArcProgressBar;
+import com.github.furkankaplan.fkblurview.FKBlurView;
 
+public class MainActivity extends AppCompatActivity {
+ColorArcProgressBar colorArcProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+colorArcProgressBar = findViewById(R.id.batteryProgressBar);
+        for (int i = 0; i <100 ; i++) {
+            colorArcProgressBar.setCurrentValues(i);
+        }
+
+
     }
 }
